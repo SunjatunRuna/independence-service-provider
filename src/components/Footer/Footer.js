61b792d50c32c8d51a824be0c72fe37ca85544de
SunjatunRuna <sunjatuna@gmail.com>
2022-04-18 15:19:17 +0600

    footer part

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='bg-info text-white p-5'>
+            <h6>Follow Us</h6>
+            <h3>
+                <i className="fa-brands fa-facebook m-2"></i>
+                <i className="fa-brands fa-instagram m-2"></i>
+                <i className="fa-brands fa-whatsapp m-2"></i>
+            </h3>
         </div>
     );
 };
