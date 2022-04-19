17f485d9c70199dbd97e851df32b883e1ea6e7c0
SunjatunRuna <sunjatuna@gmail.com>
2022-04-19 19:19:42 +0600

    toast setup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,11 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import avatar from './avatar-default-icon.png';
 import './Login.css';
 import { Link } from 'react-router-dom';
-import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import GoogleSignIn from './GoogleSignIn';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = () => {
@@ -16,26 +18,27 @@ const Login = () => {
         signInWithEmailAndPassword,
         user, error
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
 
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
 
-    const emailHandle = event =>{
+    const emailHandle = event => {
         setEmail(event.target.value);
     }
-    const passwordHandle = event =>{
+    const passwordHandle = event => {
         setPassword(event.target.value);
     }
 
-    if(user){
-        navigate(from, {replace: true});
+    if (user) {
+        navigate(from, { replace: true });
     }
     let errorMessage;
-    if(error){
+    if (error) {
         errorMessage = <p className='text-danger'>Email password didn't match</p>
     }
-    const submitButton = event =>{
+    const submitButton = event => {
         event.preventDefault()
         signInWithEmailAndPassword(email, password)
     }
@@ -46,18 +49,24 @@ const Login = () => {
             </div>
             <form onSubmit={submitButton}>
                 <p className='text-start ps-5'>Email:</p>
-                <input onBlur={emailHandle} type="email" className='p-2 w-75  mb-4' required/> <br />
+                <input onBlur={emailHandle} type="email" className='p-2 w-75  mb-4' required /> <br />
                 <p className='text-start ps-5'>Password:</p>
-                <input onBlur={passwordHandle} type="password" className='p-2 w-75 mb-2' name="password" id="" required/><br />
+                <input onBlur={passwordHandle} type="password" className='p-2 w-75 mb-2' name="password" id="" required /><br />
                 {errorMessage}
                 <button className='w-25 py-2 me-3 border border-0 px-2 rounded bg-primary text-white mt-3'>Login</button>
-                <GoogleSignIn></GoogleSignIn> 
-                <br />
-                <button className='btn btn-link border border-0 text-danger'>Forget Password?</button>
+                <GoogleSignIn></GoogleSignIn>
+                <button
+                    onClick={async () => {
+                        await sendPasswordResetEmail(email);
+                        toast('Email has been sent');
+                    }}
+                    className='btn btn-link border border-0 text-danger'>Forget Password?</button>
+                    <ToastContainer />
                 <p className='px-2'>
                     Do not have an account? <Link to='/signup'>Create new</Link>
                 </p>
             </form>
+            
         </div>
     );
 };
